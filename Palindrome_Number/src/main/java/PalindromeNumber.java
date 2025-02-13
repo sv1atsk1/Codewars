408d044eb279318b20714f4e79d32746b6a7f6dc
Viachaslau <12170068@study.bsuir.by>
2025-02-13 18:38:05 +0300

    Palindrome Number Solution

---
+++ Palindrome_Number/src/main/java/PalindromeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        isPalindrome();
+    }
+    public static void isPalindrome() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите число для проверки на полиндром");
+            while (scanner.hasNextInt()) {
+                int checkForPalindrome = scanner.nextInt();
+                if (isPalindromeNumber(checkForPalindrome)) {
+                    System.out.println("The number is palindrome");
+                } else {
+                    System.out.println("The number isn't a palindrome");
+                }
+            }
+        }
+    }
+    public static boolean isPalindromeNumber(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversed || x == reversed / 10;
+    }
+}
